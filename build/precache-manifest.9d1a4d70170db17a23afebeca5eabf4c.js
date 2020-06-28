b278b0150f73df53c36ad282afb083293e46d9c2
redfe <takayamatetsuya@takayama-no-MacBook-Pro.local>
2020-06-28 23:49:39 +0900

    add build.

---
+++ build/precache-manifest.9d1a4d70170db17a23afebeca5eabf4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "876c1e69798c9d52c612f577345dd895",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7aba4e4068371b884bf",
+    "url": "/static/css/main.dbe66cab.chunk.css"
+  },
+  {
+    "revision": "fb8d1d01499fef2fa178",
+    "url": "/static/js/2.920f7fad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.920f7fad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7aba4e4068371b884bf",
+    "url": "/static/js/main.666a4c08.chunk.js"
+  },
+  {
+    "revision": "2508e7ff816118323986",
+    "url": "/static/js/runtime-main.580e4fb4.js"
+  }
+]);
